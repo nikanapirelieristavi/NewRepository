738a98de49f2f9f1d6a7334e2433d63eed4f4a4f
nikanapirelieristavi <nikokloznapieri@gmail.com>
2023-11-26 15:37:59 +0400

    first commit

---
+++ day 6/homework6/homework6.py
@@ -0,0 +1,10 @@
+#დავპრინტოთ 1-30ამდე რიცხვები, while loop-ის გამოყენებით, კონსოლში ყოველი რიცხვის შემდეგ დაეწეროს ლუწია თუ კენტი.
+number= 1
+
+while number<=30:
+    print("the number is odd", number)
+    number += 1
+    print("the number is even", number)
+    number += 1
+
+    
